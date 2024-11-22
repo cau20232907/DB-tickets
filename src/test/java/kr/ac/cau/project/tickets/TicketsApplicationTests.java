eba1ee906062982db4b3c2d8e7dddff72567ca5c
cau20232907 <hs20232907@cau.ac.kr>
2024-11-22 11:15:06 +0900

    initial commit

---
+++ src/test/java/kr/ac/cau/project/tickets/TicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.cau.project.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
