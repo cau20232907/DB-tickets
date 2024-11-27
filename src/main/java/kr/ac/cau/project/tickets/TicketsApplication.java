c59bb45aa980c0b122ddd8412c3ce29af5f26434
cau20232907 <hs20232907@cau.ac.kr>
2024-11-28 00:53:10 +0900

    CLI base

--- src/main/java/kr/ac/cau/project/tickets/TicketsApplication.java
+++ src/main/java/kr/ac/cau/project/tickets/TicketsApplication.java
@@ -1,13 +1,30 @@
 package kr.ac.cau.project.tickets;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class TicketsApplication {
+public class TicketsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketsApplication.class, args);
 	}
 
+	/**
+	 * Callback used to run the bean.
+	 *
+	 * @param args incoming main method arguments
+	 */
+	@Override
+	public void run(String... args) {
+		//CLI 구현 위치
+		System.out.println("CLI start!");
+		Scanner input = new Scanner(System.in);
+		while (true) {
+			System.out.println(input.next());
+		}
+	}
 }
